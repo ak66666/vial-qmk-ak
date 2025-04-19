1b49c810929ae6559636274f9fc178c5c29fbcec
ak66666 <32971450+ak66666@users.noreply.github.com>
2025-04-19 12:15:52 -0400

    Converted ThumbsUp! v10 to VIAL - support Cirque touchpad.

---
+++ keyboards/thumbsup/rev10_rp2040_unibody_4x12/keymaps/default_4x12_v10_vial/config.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xDE, 0x53, 0xF6, 0x4B, 0x54, 0x6A, 0x48, 0xFE}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+
+// AK: Below are values from the regular QMK version: 
+ 
+//The number of layers for VIA/REMAP
+#define DYNAMIC_KEYMAP_LAYER_COUNT 10
